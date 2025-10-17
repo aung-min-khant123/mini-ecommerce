d6cd321789936956bf8d840d0152c37cf10903af
maythinn <developer.maythinn@gmail.com>
2025-10-17 11:25:40 +0700

    change image

--- app/data/dummyData/data.ts
+++ app/data/dummyData/data.ts
@@ -1,46 +1,65 @@
 export const carouselImages = [
-    "https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg",
-    "https://images.pexels.com/photos/425047/pexels-photo-425047.jpeg",
-    "https://images.pexels.com/photos/233698/pexels-photo-233698.jpeg",
-    "https://images.pexels.com/photos/8155/pexels-photo.jpg"
-]
+  "https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg",
+  "https://images.pexels.com/photos/425047/pexels-photo-425047.jpeg",
+  "https://images.pexels.com/photos/233698/pexels-photo-233698.jpeg",
+  "https://images.pexels.com/photos/8155/pexels-photo.jpg",
+];
 
+// export const categories = [
+//   {
+//     //   title: "Grocery",
+//       image: "/categories/cosmetics.png",
+
+//   },
+//   {
+//     //   title: "Food & Drinks",
+//       image: "/categories/perfume.png",
+
+//   },
+//   {
+//     //   title: "Office Elements",
+//       image: "/categories/furnishing.png",
+
+//   },
+//   {
+//     //   title: "Home Appliance",
+//       image: "/categories/groceries.png",
+
+//   },
+//   {
+//     //   title: "Beauty Products",
+//       image: "/categories/home-appliance.png",
+
+//   },
+//   {
+//     //   title: "Home Furnishings",
+//       image: "/categories/kitchen.png",
+
+//   }
+// ]
 export const categories = [
   {
-
-      id: 0,  
-      title: "Grocery",
-      image: "/categories/groceries.png",
-      
+    id: 0,
+    image: "/categories/cosmetics.png",
   },
   {
-      id: 1,
-      title: "Food & Drinks",
-      image: "/categories/fast-food.png",
-      
+    id: 1,
+    image: "/categories/perfume.png",
   },
   {
-      id: 2,
-      title: "Office Elements",
-      image: "/categories/web.png",
-      
+    id: 2,
+    image: "/categories/furnishing.png",
   },
   {
-      id: 3,
-      title: "Home Appliance",
-      image: "/categories/home-appliance.png",
-      
+    id: 3,
+    image: "/categories/groceries.png",
   },
   {
-      id: 4,
-      title: "Beauty Products",
-      image: "/categories/skincare.png",
-      
+    id: 4,
+    image: "/categories/home-appliance.png",
   },
   {
-      id: 5,
-      title: "Home Furnishings",
-      image: "/categories/furnishing.png",
-     
-  }
-]
+    id: 5,
+    image: "/categories/kitchen.png",
+  },
+];
